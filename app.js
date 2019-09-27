56c4432fa5d9f813ce157034893d39a1232fdd26
Tiang Cheng <tiangc@gmail.com>
2019-09-27 08:53:40 +0800

    feat: remove default path

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "public") + "/hello.html");
-});
+// app.get("/", function(req, res) {
+//   res.sendFile(path.join(__dirname, "public") + "/hello.html");
+// });
 
-app.get("*", function(req, res) {
-  res.redirect("/");
-});
+// app.get("*", function(req, res) {
+//   res.redirect("/");
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
